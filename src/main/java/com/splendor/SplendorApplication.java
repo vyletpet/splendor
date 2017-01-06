183f186ac860cc8d2976d41a377876d7224ee294
Petr Vyletelek <pvyletelek@vendavo.com>
2017-01-06 22:58:14 +0100

    First commit

---
+++ src/main/java/com/splendor/SplendorApplication.java
@@ -0,0 +1,12 @@
+package com.splendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplendorApplication.class, args);
+	}
+}
